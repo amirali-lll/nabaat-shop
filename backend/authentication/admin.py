400ed3be911a0563f372f010f7f611b3352235b9
amirali-lll <amirali.dst.lll@gmail.com>
2024-05-10 18:27:48 +0330

    feat: config admin panel for user

--- backend/authentication/admin.py
+++ backend/authentication/admin.py
@@ -1,3 +1,34 @@
 from django.contrib import admin
+from django.contrib.auth import get_user_model
+from django.contrib.auth.admin import UserAdmin
+from django.urls import reverse
+from django.utils.html import format_html
+from django.utils.translation import gettext
+from .models import ProfileImage
 
-# Register your models here.
+User = get_user_model()
+
+
+class ProfileImageInline(admin.StackedInline):
+    model = ProfileImage
+    can_delete = True
+    verbose_name_plural = gettext("Profile Image")
+    fk_name = "user"
+
+@admin.register(User)
+class UserAdmin(UserAdmin):
+    inlines = [ProfileImageInline]
+    list_display = ['image','username', 'email', 'first_name', 'last_name', 'is_active', 'is_staff', 'is_superuser']
+
+
+    def image(self, obj):
+        profile_image = obj.profile_image()
+        print(profile_image)
+        if profile_image:
+            url = reverse("admin:authentication_user_change", args=[obj.id])
+            return format_html('<a href="{}"><img src="{}" height="30" style="border-radius: 50%;"/></a>', url, profile_image)
+        else:
+            return gettext("No Image")
+    image.short_description = gettext("Image")
+
+
