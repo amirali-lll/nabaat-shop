c61edddf7ab6b9188a90735ba6b882b1de96e2a4
amirali-lll <amirali.dst.lll@gmail.com>
2024-05-15 16:12:24 +0330

    Add django-jazzmin lib to make admin panel more beautiful

--- backend/nabaatshop/settings.py
+++ backend/nabaatshop/settings.py
@@ -26,6 +26,7 @@ ALLOWED_HOSTS = ['*']
 # Application definition
 
 INSTALLED_APPS = [
+    'jazzmin',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -103,7 +104,7 @@ AUTH_PASSWORD_VALIDATORS = [
 
 AUTH_USER_MODEL = 'authentication.User'
 
-LANGUAGE_CODE = 'fa'
+LANGUAGE_CODE = 'en'
 
 TIME_ZONE = 'Asia/Tehran'
 
@@ -174,4 +175,66 @@ SWAGGER_SETTINGS = {
             'in': 'header'
         }
     },
+}
+
+JAZZMIN_SETTINGS = {
+    "site_title": "NABAAT",
+    "site_brand": "Nabaat Administration",
+    "site_logo": "core/img/logo.png",
+    "custom_css": "core/css/custom.css",
+    "welcome_sign": "Welcome to Nabaat shop",
+    "copyright": "NABAAT",
+    "related_modal_active": True,
+    "topmenu_links": [
+
+        {"name": "Home",  "url": "admin:index", "permissions": ["auth.view_user"]},
+
+        {"model": "store.order"},
+
+        {"app": "store"},
+    ],
+    "icons": {
+        "authentication": "fas fa-users-cog",
+        "authentication.user": "fas fa-user",
+        "auth.Group": "fas fa-users",
+        "store": "fas fa-store",
+        "store.Plant": "fas fa-leaf",
+        "store.Order": "fas fa-shopping-cart",
+        "store.Accessory": "fas fa-dolly",
+        "store.Category": "fas fa-stream",
+        "store.Product": "fas fa-seedling",
+    },
+}
+
+JAZZMIN_UI_TWEAKS = {
+    "navbar_small_text": False,
+    "footer_small_text": False,
+    "body_small_text": False,
+    "brand_small_text": False,
+    "brand_colour": "navbar-dark",
+    "accent": "accent-olive",
+    "navbar": "navbar-gray navbar-dark",
+    "no_navbar_border": False,
+    "navbar_fixed": True,
+    "layout_boxed": False,
+    "footer_fixed": False,
+    "sidebar_fixed": True,
+    "sidebar": "sidebar-dark-olive",
+    "sidebar_nav_small_text": False,
+    "sidebar_disable_expand": True,
+    "sidebar_nav_child_indent": False,
+    "sidebar_nav_compact_style": False,
+    "sidebar_nav_legacy_style": True,
+    "sidebar_nav_flat_style": False,
+    "theme": "default",
+    "dark_mode_theme": None,
+    "button_classes": {
+        "primary": "btn-primary",
+        "secondary": "btn-secondary",
+        "info": "btn-info",
+        "warning": "btn-warning",
+        "danger": "btn-danger",
+        "success": "btn-success"
+    },
+    "actions_sticky_top": True
 }
